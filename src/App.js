f4c866216f1a46c8763fb6c0496717b78cc6b715
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-18 16:28:30 +0700

    194. Adjusting the Custom Hook Logic

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
 import React, { useEffect, useState } from "react";
 
-import Tasks from "./components/Tasks/Tasks";
-import NewTask from "./components/NewTask/NewTask";
 import useHttp from "./components/hooks/use-http";
+import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const trasnformTasks = (taskObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const httpData = useHttp(
-    { url: "https://react-http-e2f8d-default-rtdb.firebaseio.com/tasks.json" },
-    trasnformTasks
-  );
+  const httpData = useHttp();
 
   const { isLoading, error, sendRequest: fetchTasks } = httpData;
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const trasnformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks({url : 'https://react-http-e2f8d-default-rtdb.firebaseio.com/tasks.json'}, trasnformTasks) 
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
